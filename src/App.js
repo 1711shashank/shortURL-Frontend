dc84a3d469b59f1130a2bb9b3b29e6ef150ed1f5
Kumar Shashank <1711shashank@gmail.com>
2022-03-18 02:36:45 +0530

    table

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+// import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+
+import Signup from "./Signup";
+import Login from "./Login";
+import SortURL from "./SortURL";
+import NoPageFound from "./NoPageFound";
+import Header from "./Header";
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          {/* <Route exact path="/signup" component={Signup} />
+          <Route exact path="/login" component={Login} />4
+           */}
+          <Route path="/" component={SortURL} />
+
+          {/* <Route exact path="/" component={Header} /> */}
+          {/* <Route path="/:others" component={NoPageFound} /> */}
+
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
